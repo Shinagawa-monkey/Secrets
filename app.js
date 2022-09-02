b12ed23f6209ae36167ea65a48ff99ad629144c5
Shinagawa-monkey <59675339+Shinagawa-monkey@users.noreply.github.com>
2022-09-01 23:04:09 -0500

    Edited register, login, added logout, secrets rout

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 //jshint esversion:6
+require('dotenv').config(); // used md5 instead, but need it here to hde my secret
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const ejs = require("ejs");
 const port = 3000;
 const app = express();
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: process.env.SECRET2,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 main().catch(err => console.log(err));
 async function main() {
@@ -26,69 +37,83 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/login", (req, res) => {
+app.route("/login")
+.get((req, res) => {
   res.render("login", {
     errMsg: "",
     username: "",
     password: ""
   });
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save((err) => {
-      if (!err) {
-        res.render("secrets");
-      } else {
-        res.send(err);
-      }
-    });
+})
+.post(passport.authenticate("local"), (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      res.redirect("/secrets");
+    }
   });
 });
 
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({
-    email: username
-  }, (err, foundUser) => {
+app.route("/register")
+.get((req, res) => {
+  res.render("register");
+})
+.post((req, res) => {
+  const {
+    username,
+    password
+  } = req.body;
+  User.register({
+    username: username
+  }, password, (err, user) => {
     if (err) {
-      res.send(err);
+      console.log(err);
+      res.redirect("/register");
     } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, (err, result) => {
-          if (result === true) {
-            res.render("secrets");
-            console.log("New login (" + username + ")");
-          } else {
-            res.render("login", {
-              errMsg: "Email or password incorrect",
-              username: username,
-              password: password
-            });
-          }
-        })
-      } else {
-        res.render("login", {
-          errMsg: "Email or password incorrect",
-          username: username,
-          password: password
-        });
-      }
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.get("/secrets", (req, res) => {
+  res.set(
+    'Cache-Control',
+    'no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0'
+  );
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get('/logout', function(req, res) {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.redirect('/');
     }
   });
 });
